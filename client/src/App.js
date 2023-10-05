162402f34c3bc36aa5f44f6f0e6b93467c10bbd8
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-10-05 15:13:35 +0200

    restrict routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import React, { useCallback, useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import LoggedInRoutes from "./ristrict_routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./ristrict_routes/NotLoggedInRoutes";
+import RegisteredRoutes from "./ristrict_routes/RegisteredRoutes";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <ToastContainer position="bottom-center" limit={1} />
+      <Routes>
+        <Route element={<LoggedInRoutes />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<RegisteredRoutes />}>
+          <Route path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
